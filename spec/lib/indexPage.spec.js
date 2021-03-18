8e7588a961a3b021ec38d017a1051aaacce099dc
rkiel <cygnus@pobox.com>
2021-03-18 08:06:06 -0400

    jasmine: only include css that exists

--- spec/lib/indexPage.spec.js
+++ spec/lib/indexPage.spec.js
@@ -2,6 +2,6 @@ const lib = require("../../lib/indexPage");
 
 describe("indexPage", () => {
   it("should work", () => {
-    expect(Object.keys(lib)).toEqual(["_generate", "create"]);
+    expect(Object.keys(lib)).toEqual(["_generate", "_includeCss", "create"]);
   });
 }); // indexPage
